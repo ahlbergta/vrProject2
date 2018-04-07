fa0f8e45916cb515f08603aedb245abb9e79504c
Travis Ahlberg <ahlbergta@gmail.com>
2018-04-07 14:58:11 -0500

    added character controller

---
+++ Project2/Assets/InteractionController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(this.GetComponent<Camera>().transform.position.x);
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Transform camTransform = this.GetComponent<Camera>().transform;
+            Ray ray = new Ray(camTransform.);
+        }
+	}
+}
