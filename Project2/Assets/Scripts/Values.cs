ca7aaa875aa7bea715b1ece6cc43778559eb2327
Travis Ahlberg <ahlbergta@gmail.com>
2018-04-08 14:58:32 -0500

    updated project

---
+++ Project2/Assets/Scripts/Values.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Values : MonoBehaviour {
+    public int coalCount;
+    public float temperature;
+    public float waterLevel;
+}
---
+++ Project2/Assets/Scripts/exampleScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exampleScript : MonoBehaviour {
+
+    public GameObject valueTracker;
+    private Values values;
+
+	// Use this for initialization
+	void Start () {
+        values = valueTracker.GetComponent<Values>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        // Use this to set values
+        values.temperature = 100.0f;
+	}
+}
